3f560e081aefbe5e5176e5ce71ad30039f17e037
Tyler Shepherd <100872070+TylerShep@users.noreply.github.com>
2023-04-29 03:45:18 -0600

    Create Settings.py

---
+++ DATA5330_IronWiillDB/Settings.py
@@ -0,0 +1,18 @@
+team_file = 'nfl_teams.csv'
+stadium_file = 'nfl_stadiums.csv'
+spread_file = 'spread_scores (S23).csv'
+
+
+#IWDM Postgres Keys:
+database = 'x'
+user = 'x'
+password = 'x'
+host = 'x'
+port - '5432'
+
+
+#USU MSSQL Keys:
+sever = 'x'
+databaseUSU = 'x'
+UID = 'x'
+PWD = 'x'
